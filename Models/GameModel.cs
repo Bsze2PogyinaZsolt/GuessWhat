766a3bc81f4cda7a0fc67a381d5e76ff625aa6d4
Zsolt Pogyina <bsze2pogzso@vasvari.org>
2023-10-11 13:15:54 +0200

    GameModel elkezdése

--- Models/GameModel.cs
+++ Models/GameModel.cs
@@ -8,5 +8,21 @@ namespace GuessWhat.Models
 {
     public class GameModel
     {
+        private Random random = new Random();
+        private int randomNumber;
+
+        public void StartNewGame()
+        {
+            randomNumber = random.Next(1, 101);
+        }
+
+        public string CheckGuess(int guess)
+        {
+            if (guess < randomNumber)
+                return "Higher";
+            else if (guess > randomNumber) 
+                return "Lower";
+            else return "correct"
+        }
     }
 }
