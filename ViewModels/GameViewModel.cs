f3de27b72a0afc81d1f3d7ec5800d7068876fae3
Zsolt Pogyina <bsze2pogzso@vasvari.org>
2023-10-12 17:14:07 +0200

    viewmodelben a folyamatok meghatározása

--- ViewModels/GameViewModel.cs
+++ ViewModels/GameViewModel.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 using GuessWhat.Models;
+using ViewModels.BaseClass;
 
 namespace GuessWhat.ViewModels
 {
-    internal class GameViewModel
+    public class GameViewModel
     {
+        private GameModel _game;
+        public int Guess { get; set; }
+        public string Feedback { get; set; }
+
+        public ICommand CheckGuessCommand { get; set; }
+        public ICommand StartNewGameCommand { get; set; }
+
+        public GameViewModel()
+        {
+            game = new GameModel();
+            StartNewGame();
+            CheckGuessCommand = new RelayCommand(CheckGuess);
+            StartNewGameCommand = new RelayCommand(StartNewGame);
+
+
+        }
+
+        private void CheckGuess()
+        {
+            Feedback = game.CheckGuess(Guess);
+        }
+
+        private void StartNewGame()
+        {
+            game.StartNewGame();
+            Guess = 0;
+            Feedback = "Guess the number!";
+        }
+    
     }
 }
